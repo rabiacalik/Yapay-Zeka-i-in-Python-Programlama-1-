099f2b6b7607d387e941785f57d4985b43f1f0e6
Rabia Çalık <115617420+rabiacalik@users.noreply.github.com>
2023-10-25 10:44:57 +0300

    ww

--- python_temelleri/variables.py
+++ python_temelleri/variables.py
@@ -24,14 +24,82 @@ variable_len = len(s)
 print(variable_type)
 print(variable_len)
 
-
+# bu sekilde tanımlamalar yapilabiliyor
 cevap = '5'*5
+
 # %% Numbers
 
 integer_var = -10
 
 #float - double
 double_var = 10.0
+
+# %% Built in Function
+
+#float(10)
+#print(kelime)
+
+# %% User Defined Function
+
+i = 10
+j = 10
+
+def my_first_function(var_i, var_j):
+    """
+    bu kisima aciklamalar yazabilirim
+
+    Returns
+    -------
+    None.
+
+    """
+    output = var_i + var_j
+    return output
+
+return_output = my_first_function(i, j)
+
+print("sonuc = ", return_output)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
