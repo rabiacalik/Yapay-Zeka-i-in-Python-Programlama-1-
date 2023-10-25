29d94957262088bd0c9a9194ff6af0f4686adab5
Rabia Çalık <115617420+rabiacalik@users.noreply.github.com>
2023-10-25 10:24:07 +0300

    yeni

--- python_temelleri/variables.py
+++ python_temelleri/variables.py
@@ -4,5 +4,34 @@ Spyder Editor
 
 This is a temporary script file.
 """
+# %% Variables
+var1 = 10
+var2 = 20
 
-a
+gun = "pazartesi"
+
+#bu sekilde farkli bolumlendirmeler yapabiliyorsun
+# %% String
+
+s = "merhabalar efenim.."
+
+# tipini doner
+variable_type = type(s)
+
+# uzunlugunu doner
+variable_len = len(s)
+
+print(variable_type)
+print(variable_len)
+
+
+cevap = '5'*5
+# %% Numbers
+
+integer_var = -10
+
+#float - double
+double_var = 10.0
+
+
+
