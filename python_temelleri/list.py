8b8e2ae5ea9d39f483f57843d3b7364afd37aba9
Rabia Çalık <115617420+rabiacalik@users.noreply.github.com>
2023-10-26 07:46:40 +0300

    yeni

--- python_temelleri/list.py
+++ python_temelleri/list.py
@@ -44,6 +44,51 @@ print(list2)
 # bir liste farkli veri turleri tutabilir
 list3 = [1, 2, "rabia", "kopkop"]
 
+# %% Tuple
+
+t = (1, 2, 3, 4, 5)
+
+# %% Dictionary
+
+dictionary = {"ali":12, "ayse":15, "mehmet":30}
+# keys = ali, ayse, mehmet
+# value = 12, 15, 30
+
+print(dictionary.keys())
+print(dictionary.values())
+
+# %% Conditionals
+# if else statement
+
+var1 = 10
+var2 = 20
+
+if (var1 > var2):
+    print("var1, var2 den buyuktur")
+elif (var1 < var2):
+    print("var1, var2 den kucuktur")
+else:
+    print("var1, var2 ye esittir")
+
+
+liste = [1, 2, 3, 4, 5]
+
+if 6 in liste:
+    print("evet, 6 degeri icerisinde")
+else:
+    print("hayir, 6 degeri icerisinde degil")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
